acd18c353cab3aace7ccc844c0caeec64a1bd3b3
Clément GARNIER <mail@garnierc.fr>
2013-01-22 00:20:18 -0500

    Integrated contact form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,19 +2,35 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
   
-  def index = Action {
+  def index = Action { implicit request =>
     Ok(views.html.index())
   }
 
-  def projects = Action {
+  def projects = Action { implicit request =>
     Ok(views.html.projects())
   }
 
-  def contact = Action {
+  def contact = Action { implicit request =>
     Ok(views.html.contact())
   }
-  
+
+  val sendForm = Form(
+    tuple(
+      "name" -> nonEmptyText,
+      "email" -> email,
+      "message" -> nonEmptyText
+    )
+  )
+
+  def sendMessage = Action { implicit request =>
+    sendForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(formWithErrors.errorsAsJson),
+      value => Ok
+    ) 
+  }
 }
