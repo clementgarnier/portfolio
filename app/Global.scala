103d2a9ed21b7f7fbb9c84a8ba7fb6630a1fb763
Clément GARNIER <mail@garnierc.fr>
2013-01-22 02:35:45 -0500

    Added message handler

--- app/Global.scala
+++
@@ -1,8 +0,0 @@
-import play.api._
-import play.api.data._
-import play.api.mvc._
-import play.api.mvc.Results._
-
-object Global extends GlobalSettings {
-
-}
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.Play.current
 
 object Application extends Controller {
   
@@ -30,7 +31,24 @@ object Application extends Controller {
   def sendMessage = Action { implicit request =>
     sendForm.bindFromRequest.fold(
       formWithErrors => BadRequest(formWithErrors.errorsAsJson),
-      value => Ok
-    ) 
+      value => value match {
+        case (name: String, email: String, message: String) => {
+          current.configuration.getString("mailer.recipient") match {
+            case Some(recipient) => { 
+              import com.typesafe.plugin._
+              val mail = use[MailerPlugin].email
+
+              mail.setSubject("Message from " + name +" <"+ email +">")
+              mail.addRecipient(recipient)
+              mail.addFrom(name +" <"+ email +">")
+              mail.send(message)
+
+              Ok
+            }
+            case _ => throw new RuntimeException("Mailer recipient is not properly configured in application.conf")
+          }
+        }
+      }
+    )
   }
 }
